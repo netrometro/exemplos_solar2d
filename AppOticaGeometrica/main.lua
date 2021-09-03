63cac6dffd61072a4c7647d0ca1a07bb20acb40c
Ewerton Mendonça <netrometro@yahoo.com.br>
2021-09-03 11:45:20 -0300

    Scene00 como início

--- AppOticaGeometrica/main.lua
+++ AppOticaGeometrica/main.lua
@@ -9,4 +9,4 @@ local composer = require( "composer" )
 -- Code to initialize your app
 
 -- Assumes that "scene1.lua" exists and is configured as a Composer scene
-composer.gotoScene( "scene07" , { effect = "fade" , time = 500 } )
+composer.gotoScene( "scene00" , { effect = "fade" , time = 500 } )
